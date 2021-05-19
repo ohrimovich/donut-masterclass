7660fcdc02097d3a6a0620cf67a7857b3e80ae7b
Mark <yakushyn.mark@gmail.com>
2021-05-19 17:50:04 +0300

    Reviews/slider/
    текст и пагинация

--- src/index.js
+++ src/index.js
@@ -5,3 +5,51 @@ import show from './js/showMore';
 
 refs.aboutButton.addEventListener('click', show);
 refs.programButton.addEventListener('click', show);
+
+
+
+
+
+var helpers = {
+	addZeros: function (n) {
+		return (n < 10) ? '0' + n : '' + n;
+	}
+};
+
+function sliderInit() {
+  var $slider = $('.slider-holder');
+  $slider.each(function() {
+    var $sliderParent = $(this).parent();
+    $(this).slick({
+      
+      slidesToShow: 1,
+      slidesToScroll: 1,
+        dots: false,
+      arrows: false,
+      infinite: true,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            adaptiveHeight: true
+          }
+        }
+      ]
+    });
+
+    if ($(this).find('.item').length > 1) {
+      $(this).siblings('.slides-numbers').show();
+    }
+
+    $(this).on('afterChange', function(event, slick, currentSlide){
+      $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
+    });
+
+    var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
+    $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
+
+  });
+ 
+}
+
+sliderInit();
