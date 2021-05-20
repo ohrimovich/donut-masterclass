66abc58f543006bf4a54056ba11d462f7350b08f
Oleksandr Okhrimovich <ohrimovich.sanek@gmail.com>
2021-05-20 17:17:41 +0300

    Added  functional read more for About us

--- src/js/showMore.js
+++ src/js/showMore.js
@@ -1,24 +1,11 @@
-// import refs from './refs';
-// export default evt => {
-//   if (evt.currentTarget.classList.contains('content__btn-more')) {
-//     if (refs.abouMoreContent.classList.contains('moreContentIsHiden')) {
-//       refs.abouMoreContent.classList.remove('moreContentIsHiden');
-//       refs.aboutButton.textContent = 'Less';
-//       return;
-//     }
-//     refs.abouMoreContent.classList.add('moreContentIsHiden');
-//     refs.aboutButton.textContent = 'Read more';
-//   }
-  // if (evt.currentTarget.classList.contains('program__button')) {
-  //   if (refs.programMoreContent.classList.contains('program__more--hidden')) {
-  //     refs.programMoreContent.classList.remove('program__more--hidden');
-  //     refs.programButton.textContent = 'Less';
-  //     return;
-  //   }
-  //   refs.programMoreContent.classList.add('program__more--hidden');
-  //   refs.programButton.textContent = 'Read more';
-  // }
-// };
+$('.content__btn-more').click(function() {
+  $('.moreContentIsHiden').slideToggle();
+  if ($('.content__btn-more').text() == "Read more") {
+    $(this).text("Less")
+  } else {
+    $(this).text("Read more")
+  }
+});
 
 $('.program__button').click(function() {
   $('.program__more').slideToggle();
@@ -28,3 +15,6 @@ $('.program__button').click(function() {
     $(this).text("Read more")
   }
 });
+
+
+
