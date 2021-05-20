cbc384c60ee02a970514a74087044d7bb2d189b2
AndrewMozharovsky <71605765+AndrewMozharovsky@users.noreply.github.com>
2021-05-20 15:21:43 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './sass/main.scss';
 import './js/menu';
 import refs from './js/refs';
 import show from './js/showMore';
+import './js/slider';
 
 refs.aboutButton.addEventListener('click', show);
 refs.programButton.addEventListener('click', show);
