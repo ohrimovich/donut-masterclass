28976a4ad553ae809130d544b40f13caa26f2823
Mark <yakushyn.mark@gmail.com>
2021-05-21 12:55:09 +0300

    reviews/slider
    final test

--- src/index.js
+++ src/index.js
@@ -2,58 +2,7 @@ import './sass/main.scss';
 import './js/menu';
 import refs from './js/refs';
 import show from './js/showMore';
+import './js/slider';
 
 refs.aboutButton.addEventListener('click', show);
 refs.programButton.addEventListener('click', show);
-
-
-
-
-
-
-
-
-
-var helpers = {
-	addZeros: function (n) {
-		return (n < 10) ? '0' + n : '' + n;
-	}
-};
-
-function sliderInit() {
-  var $slider = $('.slider-holder');
-  $slider.each(function() {
-    var $sliderParent = $(this).parent();
-    $(this).slick({
-       asNavFor: '.foto-slider',
-      slidesToShow: 1,
-      slidesToScroll: 1,
-        dots: false,
-      arrows: false,
-      infinite: false,
-      responsive: [
-        {
-          breakpoint: 767,
-          settings: {
-            adaptiveHeight: true
-          }
-        }
-      ]
-    });
-
-    if ($(this).find('.item').length > 1) {
-      $(this).siblings('.slides-numbers').show();
-    }
-
-    $(this).on('afterChange', function(event, slick, currentSlide){
-      $sliderParent.find('.slides-numbers .active').html(helpers.addZeros(currentSlide + 1));
-    });
-
-    var sliderItemsNum = $(this).find('.slick-slide').not('.slick-cloned').length;
-    $sliderParent.find('.slides-numbers .total').html(helpers.addZeros(sliderItemsNum));
-
-  });
- 
-}
-
-sliderInit();
